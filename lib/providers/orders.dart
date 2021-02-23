f1e19c48f16f91c5d9d6609fa26dd28d286063c2
omar2432 <omar.ahmed2432@gmail.com>
2021-02-23 06:10:32 +0200

    edited firebase URL

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -27,7 +27,7 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> fetchAndSetOrders() async {
-    const url = 'https://flutter-update.firebaseio.com/orders.json';
+    const url = 'https://fireeats-434d3.firebaseio.com/orders.json';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -43,11 +43,11 @@ class Orders with ChangeNotifier {
           products: (orderData['products'] as List<dynamic>)
               .map(
                 (item) => CartItem(
-                      id: item['id'],
-                      price: item['price'],
-                      quantity: item['quantity'],
-                      title: item['title'],
-                    ),
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title'],
+                ),
               )
               .toList(),
         ),
@@ -58,7 +58,7 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    const url = 'https://flutter-update.firebaseio.com/orders.json';
+    const url = 'https://fireeats-434d3.firebaseio.com/orders.json';
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
